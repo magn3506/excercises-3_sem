530f284a9a053c9611758279827e77cdcd23b236
Magnus Vagn Jensen <magnusvagnjensen@Magnuss-MBP-3.domain_not_set.invalid>
2019-02-11 11:06:38 +0100

    commit all

---
+++ script.js
@@ -0,0 +1,25 @@
+console.log("hej");
+
+const name = "peter";
+const animalType = "cat";
+const animalName = "Mandu";
+const len = name.length;
+const letter = name[0];
+
+const str1 = "     There is    space here /n     ";
+const str2 = str1.trim();
+
+const fullName = "Peter Heronimous Lind";
+const firstName = fullName.substring(0, 5);
+
+console.log(`My name is ${name}, i have a ${animalType} called ${animalName} `);
+
+console.log(`${name} is ${len} charachters long`);
+
+console.log(`The first letter of ${name} is ${letter}`);
+
+console.log(str1);
+console.log(str2);
+
+console.log(fullName);
+console.log(firstName);
